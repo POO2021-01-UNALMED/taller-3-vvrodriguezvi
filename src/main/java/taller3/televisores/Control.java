912538b3a0f290225324b5a1609b6eb618e91104
Vanessa Rodriguez <vvrodriguezvi@unal.edu.co>
2021-03-16 19:27:51 -0500

    Taller3

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,51 @@
+package taller3.televisores;
+
+public class Control {
+	TV tv;
+
+	public TV getTv() {
+		return tv;
+	}
+
+	public void setTv(TV tv) {
+		this.tv = tv;
+		
+	}
+
+	public Control(TV tv) {
+		this.tv = tv;
+		TV.numTV++;
+		
+	}
+	
+	// metodos compartidos con tv
+	
+	public void turnOn() {
+		this.tv.turnOff(tv.getEstado());
+	}
+	public void turnOff() {
+		this.tv.turnOff(tv.getEstado());
+	}
+	public void  canalUp() {
+		this.tv.canalUp(tv.getCanal());
+	}
+	public void  canalDown() {
+		this.tv.canalDown(tv.getCanal());
+	}
+	public void volumenUp() {
+		this.tv.volumenUp(tv.getVolumen());
+	}
+	public void volumenDownn() {
+		this.tv.volumenDown(tv.getVolumen());
+	}
+	public void setCanal() {
+		this.tv.setCanal(tv.canal);
+	}
+	
+	// metodos enlazar
+	
+	public void enlazar(TV tv) {
+		this.tv = tv;
+		
+	}
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,120 @@
+package taller3.televisores;
+
+public class TV {
+	Marca marca;
+	int canal = 1;
+	int precio = 500;
+	boolean estado;
+	int volumen = 1;
+	Control control;
+	static int numTV;
+
+	
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca = marca;
+		this.estado = estado;
+		TV.numTV++;
+		
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	public int getCanal() {
+		return canal;
+	}
+
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public int getVolumen() {
+		return volumen;
+	}
+
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+
+	public Control getControl() {
+		return control;
+	}
+
+	public void setControl(Control control) {
+		this.control = control;
+	}
+
+	public static int getNumTV() {
+		return numTV++;
+	}
+
+	public static void setNumTV(int numTV) {
+		TV.numTV = numTV;
+	}
+	
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	public boolean getEstado() {
+		return estado;
+	}
+
+	
+	public void turnOn(boolean estado) { // Encender 
+		if(this.estado == false) {
+			setEstado(true);
+			return;
+			
+		}
+	}
+	public void turnOff(boolean estado) {
+		if(this.estado == true) {
+			setEstado(false);
+			return;
+			
+		}
+	}
+	public void canalUp(int canal){
+		if(this.estado =  true) {
+			if(this.canal > 120 && this.canal < 1 ) {
+				this.canal++;
+			}
+		}
+	}
+	public void canalDown(int canal){
+		if(this.estado=  true) {
+			if(this.canal > 120 && this.canal < 1 ) {
+				this.canal--;
+			}
+		}
+	}
+	public void volumenUp(int volumen){
+		if(this.estado = true) {
+			if(this.volumen > 7 && this.volumen < 1 ) {
+				this.volumen++;
+			}		
+		}
+		
+	}
+	public void volumenDown(int volumen){
+		if(this.estado = true) {
+			if(this.volumen > 7 && this.volumen < 1 ) {
+				this.volumen--;
+			}
+		}
+	}
+}
